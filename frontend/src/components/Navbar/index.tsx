83ee7a375c58b8d4e872d850dc65f72e1d1277af
Ivan Colombo <68520525+ivancolombo@users.noreply.github.com>
2022-01-15 13:12:00 -0300

    Update index.tsx

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -10,7 +10,7 @@ function Navbar() {
           <a href="https://github.com/ivancolombo" target="_blank" rel="noreferrer">
             <div className="dsmovie-contact-container">
               <GithubIcon />
-              <p className="dsmovie-contact-link">/Ivan Colombo</p>
+              <p className="dsmovie-contact-link">/ivancolombo</p>
             </div>
           </a>
         </div>
